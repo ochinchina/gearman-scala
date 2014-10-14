65565664533c05103a7192ad3e814b523a71389f
ochinchina <ochinchina@gmail.com>
2014-10-14 12:19:08 +0800

    add license info & worker graceful shutdown

--- src/main/scala/org/gearman/worker/GearmanWorker.scala
+++ src/main/scala/org/gearman/worker/GearmanWorker.scala
@@ -1,3 +1,22 @@
+/*
+* Copyright (c) 2014 by its authors. Some rights reserved.
+* See the project homepage at
+*
+* https://github.com/ochinchina/gearman-scala
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.gearman.worker
 
 import org.gearman.message._
@@ -9,62 +28,169 @@ import java.net.InetSocketAddress
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 
-trait WorkResponser {
-	def data( data: String	 )
+/**
+ * responser for a job got from gearman server
+ * 
+ * @author Steven Ou  
+ */ 
+trait JobResponser {
+	/**
+	 * send data back to client
+	 *  
+	 * @param data the data to be sent	 	 
+	 */	 	
+	def data( data: String )
+	
+	/**
+	 * send the status of the work to the client
+	 * 
+	 * @param numerator the percentage of numerator about the job
+	 * @param denominator the percentage of denominator about the job 	 	 	 
+	 */	 	
 	def status( numerator: Int, denominator: Int )
+	
+	/**
+	 * send a warning data to client
+	 * 
+	 * @param data the warning data	 	 
+	 */	 	
 	def warning( data: String )
+	
+	/**
+	 * indicate the job is completed
+	 * 
+	 * @param data the data to be sent to client when the job completed	 	 
+	 */	 	
 	def complete( data: String )
+	
+	/**
+	 *  indicate the job is failed
+	 */	 	
 	def fail
+	
+	/**
+	 *  indicate exception occurs when processing the job
+	 *  
+	 * @data the exception data	 	 
+	 */	 	
 	def exception( data: String )
 }
 
-trait WorkFuncHandler {
+/**
+ * define interface to process the job got from gearman server
+ *  
+ * @author Steven Ou
+ */  
+trait JobHandler {
+	/**
+	 *  handle the received job
+	 *  
+	 * @param jobHandle the job handle
+	 * @param funcName the function name of the job
+	 * @param data the job data
+	 * @param uid the job unique id setting by client side
+	 * @param responser the job response interface, the job handler must send
+	 * any feed back to client through this interface	  	 	 	 	 	 	 
+	 */	 	
 	def handle( jobHandle: String, 
 				funcName: String, 
 				data: String, 
 				uid: Option[String],
-				responser: WorkResponser )
+				responser: JobResponser )
 }
 
+/**
+ * this class manages all the on-going jobs fetched from the gearman server side
+ * 
+ * @author Steven Ou  
+ */ 
 class JobList {
 	val jobs = new HashMap[ String, MessageChannel ]
+	val channelJobs = new HashMap[ MessageChannel, java.util.LinkedList[ String ] ]
 	
+	/**
+	 *  add a job to the list
+	 *  
+	 * @param jobHandle the job handle
+	 * @param channel the message channel of gearman server	 	 	 
+	 */	 	
 	def addJob( jobHandle: String, channel: MessageChannel ) {
-		jobs.synchronized { jobs += jobHandle -> channel }
+		jobs.synchronized { 
+			jobs += jobHandle -> channel
+			if( !channelJobs.contains( channel ) ) channelJobs += channel -> new java.util.LinkedList[ String ]
+			channelJobs( channel ).add( jobHandle )
+		}
 	}
 	
+	/**
+	 * remove a job by the job handle
+	 * 
+	 * @param jobHandle the job handle	 	 
+	 */	 	
 	def removeJob( jobHandle: String ) {
-		jobs.synchronized { jobs -= jobHandle }
+		jobs.synchronized {
+			val channel = jobs.get( jobHandle ) 
+			jobs -= jobHandle
+			if( channel.nonEmpty && channelJobs.contains( channel.get ) ) {
+				channelJobs( channel.get ).remove( jobHandle )
+				if( channelJobs( channel.get ).isEmpty ) channelJobs -= channel.get
+			}
+		}
 	}
 	
+	/**
+	 * get the number of total jobs
+	 *  
+	 * @return the number of total jobs	 	 
+	 */	 	
 	def size = jobs.synchronized{ jobs.size }
+
+	/**
+	 *  get the number of jobs on the gearman server channel
+	 *  
+	 * @param channel the gearman server channel	 	 
+	 */	 		
+	def size( channel: MessageChannel ) = jobs.synchronized {
+		if( channelJobs.contains( channel ) ) channelJobs( channel ).size else 0
+	} 
 
 }
 
-class DefWorkResponser( jobHandle: String, channel: MessageChannel, jobCompleted: =>Unit ) extends WorkResponser {
+/**
+ * default job responser implementation
+ * 
+ * @param jobHandle the job handle to indicate which job this reponse is for
+ * @param channel the gearman server channel
+ * @param jobCompleted the callback for job completion. If the job is completed,
+ * the jobCompleted callback will be called
+ * 
+ * @author Steven Ou       
+ */ 
+class DefJobResponser( jobHandle: String, channel: MessageChannel, jobCompleted: =>Unit ) extends JobResponser {
 
-	def data( data: String ) {
+	override def data( data: String ) {
 		channel.send( new WorkDataReq( jobHandle, data ) )
 	}
 	
-	def status( numerator: Int, denominator: Int ) {
+	override def status( numerator: Int, denominator: Int ) {
 		channel.send( new WorkStatusReq( jobHandle, numerator, denominator ) )
 	}
 	
-	def complete( data: String ) {
+	override def complete( data: String ) {
 		jobCompleted
 		channel.send( new WorkCompleteReq( jobHandle, data ) )
 	}
-	def warning( data: String ) {
+	
+	override def warning( data: String ) {
 		channel.send( new WorkWarningReq( jobHandle, data ) )
 	}
 	
-	def fail {
+	override def fail {
 		jobCompleted
 		channel.send( new WorkFailReq( jobHandle ) )
 	}
 	
-	def exception( data: String ) {
+	override def exception( data: String ) {
 		jobCompleted
 		channel.send( new WorkExceptionReq( jobHandle, data ) )
 	}
@@ -78,28 +204,63 @@ class DefWorkResponser( jobHandle: String, channel: MessageChannel, jobCompleted
  * format. If multiple gearman servers are provided, the worker will try to connect to 
  * all the gearman servers and get the job from them
  *
- * @param maxOnGoingJobs max number of jobs can be processed concurrently by worker       
+ * @param maxOnGoingJobs max number of jobs can be processed concurrently by worker
+ * 
+ * @author Steven Ou         
  */  
-class GearmanWorker( servers: String, maxOnGoingJobs: Int ) {
-	val funcHandlers = new HashMap[ String, WorkFuncHandler ]
+class GearmanWorker( servers: String, var maxOnGoingJobs: Int ) {
+	val funcHandlers = new HashMap[ String, JobHandler ]
 	val serverAddrs = parseAddressList( servers )
 	val channels = new java.util.LinkedList[MessageChannel]
-	
 	// all the on-going jobs
 	val jobs = new JobList
-		
-	def registerHandler( funcName: String, handler: WorkFuncHandler ) {
+
+	@volatile	
+	var stopped = false
+	
+	/**
+	 *  register a function handler. Function handler can be registered
+	 *  at any time	 
+	 *  
+	 * @param funcName the function name
+	 * @param handler the handler to process the function	  	 	 
+	 */	 		
+	def registerHandler( funcName: String, handler: JobHandler ) {
 		funcHandlers.synchronized { funcHandlers += funcName -> handler }
 		broadcast( new CanDo( funcName ) )
 	}
 	
+	/**
+	 *  unregister a function handler by function name 
+	 *  
+	 * @param funcName the function name
+	 */	
 	def unregisterHandler( funcName: String ) {
 		funcHandlers.synchronized { funcHandlers -= funcName }
 		broadcast( new CantDo( funcName ) )
 	}	
 	
-	def start() {		
+	/**
+	 *  start the gearman worker
+	 */	 	
+	def start() {
+		stopped = false		
 		for( i <- 0 until serverAddrs.size ) start( serverAddrs( i ) )			
+	}
+	
+	/**
+	 *  shutdown the worker
+	 *  
+	 * @param graceful do graceful shutdown	 	 
+	 */	 	
+	def shutdown( graceful: Boolean ) {
+		stopped = true
+		if( !graceful ) channels.synchronized {
+			val iter = channels.iterator
+			while( iter.hasNext ) try { iter.next.close } catch { case e: Throwable => }
+			channels.clear 
+		} 
+		
 	}
 	
 	private def start( addr: InetSocketAddress ) {		
@@ -112,8 +273,8 @@ class GearmanWorker( servers: String, maxOnGoingJobs: Int ) {
 				funcHandlers.synchronized { 		
 					funcHandlers.foreach( p => channel.send( new CanDo( p._1 ) ) )
 				}				
-				channel.start						
-				channel.send( new GrabJob )	
+				channel.open
+				grabJob( channel )						
 			}
 		} )
 	}
@@ -141,7 +302,7 @@ class GearmanWorker( servers: String, maxOnGoingJobs: Int ) {
 				case Some( handler ) => 
 					jobs.addJob( jobHandle, from )
 					val completeCb = { handleJobCompleted( from, jobHandle ) }					
-					future { handler.handle( jobHandle, funcName, data, uid, new DefWorkResponser( jobHandle, from, completeCb ) ) }
+					future { handler.handle( jobHandle, funcName, data, uid, new DefJobResponser( jobHandle, from, completeCb ) ) }
 				case _ => from.send( new Error( "2", "No handler found") )
 			}
 		}
@@ -151,11 +312,14 @@ class GearmanWorker( servers: String, maxOnGoingJobs: Int ) {
 	
 	private def handleJobCompleted( channel: MessageChannel, jobHandle: String ) {
 		jobs.removeJob( jobHandle )
-		grabJob( channel )
+		if( stopped ) {
+			if( jobs.size( channel ) <= 0 ) 
+				try { channel.close } catch{ case e: Throwable => }
+		} else grabJob( channel )
 	}
 	
 	private def grabJob( channel: MessageChannel ) {
-		if( maxOnGoingJobs <= 0 || jobs.size < maxOnGoingJobs ) channel.send( new GrabJob )
+		if( maxOnGoingJobs <= 0 || jobs.size < maxOnGoingJobs ) channel.send( new GrabJob ) else channel.send( new PreSleep )
 	}
 	
 	private def broadcast( msg: Message ) {
@@ -166,25 +330,4 @@ class GearmanWorker( servers: String, maxOnGoingJobs: Int ) {
 			}
 		}
 	} 
-}
-
-object GearmanWorker {
-	def main( args: Array[String] ) {
-		val worker = new GearmanWorker( "127.0.0.1:3333,127.0.0.1:3334", 10 )
-		worker.registerHandler( "test", new WorkFuncHandler {
-			def handle( jobHandle: String, 
-				funcName: String, 
-				data: String, 
-				uid: Option[String],
-				responser: WorkResponser ) {
-					responser.complete( "no data" )
-				}
-		})
-		
-		worker.start
-		
-		while( true ) {
-			Thread.sleep( 1000 )
-		}		
-	}
 }
